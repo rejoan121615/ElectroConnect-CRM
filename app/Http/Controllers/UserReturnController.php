7650a487bb1eb42228cba55691b54f33c2c36bd1
Mohd Rejoan <wearecrx@gmail.com>
2023-08-09 01:57:23 +0600

    all the database model, request, controller, migration file added

---
+++ app/Http/Controllers/UserReturnController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserReturn;
+use App\Http\Requests\StoreUserReturnRequest;
+use App\Http\Requests\UpdateUserReturnRequest;
+
+class UserReturnController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserReturnRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserReturn $userReturn)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UserReturn $userReturn)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserReturnRequest $request, UserReturn $userReturn)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserReturn $userReturn)
+    {
+        //
+    }
+}
